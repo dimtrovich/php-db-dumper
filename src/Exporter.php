94df9531d6b7fe9d38341ec3d41adf60fa38a704
Dimitri Sitchet Tomkeu <dimitrisitchet@gmail.com>
2024-09-27 19:52:59 +0200

    feat: possibility for the programmer to add information to the header of the backup file

--- src/Exporter.php
+++ src/Exporter.php
@@ -207,18 +207,27 @@ class Exporter
         $header = '';
 
         if (! $this->option->skip_comments) {
+			$divider = str_repeat('----------------------------------------------------', 2);
+
             // Some info about software, source and time
-            $header = '-- Database Backup Manager' . PHP_EOL .
+            $header = '-- ' . $divider . PHP_EOL . '-- ' . PHP_EOL .
+					'-- Database Backup Manager' . PHP_EOL .
                     '-- This backup was created automatically by the Dimtrovich Db-Dumper. A simplest PHP Database Backup Manager' . PHP_EOL .
                     '-- © ' . date('Y') . ' Dimitri Sitchet Tomkeu' . PHP_EOL .
                     '-- https://github.com/dimtrovich/php-db-dumper' . PHP_EOL .
                     '-- ' . PHP_EOL .
                     '-- Host: ' . $this->pdo->getAttribute(PDO::ATTR_CONNECTION_STATUS) . PHP_EOL .
                     "-- Database: {$this->database}" . PHP_EOL .
-                    '-- Server version: ' . $this->pdo->getAttribute(PDO::ATTR_SERVER_VERSION) . ' Driver: ' . $this->pdo->getAttribute(PDO::ATTR_DRIVER_NAME) . PHP_EOL .
-                    '-- ' . PHP_EOL .
-                    '-- Generated on: ' . date('r') . PHP_EOL .
-                    '-- ' . str_repeat('----------------------------------------------------', 2) . PHP_EOL . PHP_EOL;
+                    '-- Server version: ' . $this->pdo->getAttribute(PDO::ATTR_SERVER_VERSION) . ' Driver: ' . $this->pdo->getAttribute(PDO::ATTR_DRIVER_NAME) . PHP_EOL;
+
+			if (! $this->option->skip_dump_date) {
+				$header .= '-- ' . PHP_EOL . '-- Generated on: ' . date('r') . PHP_EOL;
+			}
+			if ('' !== $this->option->message) {
+				$header .= '-- ' . PHP_EOL . trim($this->option->message) . PHP_EOL;
+			}
+
+			$header .= '-- ' . PHP_EOL . $divider . PHP_EOL . PHP_EOL;
         }
 
         return $header;
--- src/Option.php
+++ src/Option.php
@@ -57,8 +57,11 @@ final class Option
     public bool $skip_dump_date          = false;
     public bool $skip_definer            = false;
 
-    // deprecated
-    public bool $disable_foreign_keys_check = true;
+	/**
+	 * Customised user message to be inserted in the header of the dumped file
+	 */
+	public string $message = '';
+
     private array $options                  = [];
 
     public function __construct(array $options)
@@ -84,6 +87,10 @@ final class Option
     {
         foreach ($options as $key => $val) {
             if (property_exists($this, $key)) {
+				if ($key === 'message' && $val !== '' && ! str_starts_with($val, '-- ')) {
+					$val = '-- ' . $val;
+				}
+
                 $this->{$key} = $val;
                 unset($options[$key]);
             }
